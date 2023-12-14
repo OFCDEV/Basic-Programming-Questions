15e1c843c168362178f055393729dbfd970d2c56
Debasish Bhola <devop69@gmail.com>
2023-12-14 19:54:59 +0530

    main method completed

--- copyArray.c
+++ copyArray.c
@@ -14,5 +14,21 @@ bool isInArray(int arr[],int size,int value){
     
 }
 void main(){
+    int arr[] = {4,7,7,3,2,5,5};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int res[size];
+    int count = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if(!isInArray(res,size,arr)){
+            res[count++] = arr[i];
+        }
+    }
+    printf("Resulting Array:\n");
+    for (int i = 0; i < count; i++)
+    {
+        printf("%d",res[i]);
+    }
+    
     
 }
