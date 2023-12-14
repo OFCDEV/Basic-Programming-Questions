637fea218cecf16064f5df134269082aeb84550d
Debasish Bhola <devop69@gmail.com>
2023-12-14 13:56:33 +0530

    main method fixed

--- arrayRev.c
+++ arrayRev.c
@@ -6,6 +6,8 @@ void arrayRev(int arr[],int start, int end){
         temp = arr[start];
         arr[start] = arr[end];
         arr[end]= temp;
+        start++;
+        end--;
     }
     
 }
@@ -21,4 +23,10 @@ void printArray(int arr[], int size){
 void main(){
     int arr[] = {1,2,3,4,5,6};
     int n = sizeof(arr) / sizeof(arr[0]);
+    printf("Before reversing the elements are:\n");
+    printArray(arr,n);
+    printf("\nAfter reversing the elements are:\n");
+    arrayRev(arr,0,n-1);
+    printArray(arr,n);
+
 }
