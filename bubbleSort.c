715893f8a571c33caa349f8c1632ad8b53c82008
Debasish Bhola <devop69@gmail.com>
2023-12-14 18:34:30 +0530

    Completed

--- bubbleSort.c
+++ bubbleSort.c
@@ -8,8 +8,8 @@ void sort(int arr[], int n)
             if (arr[j] > arr[j + 1])
             {
                 int temp = arr[j];
-                arr[j] = arr[j - 1];
-                arr[j - 1] = temp;
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
     }
@@ -18,14 +18,16 @@ void printArray(int arr[],int n){
     for (int i = 0; i < n; i++)
     {
         printf("%d",arr[i]);
+        printf("\n");
     }
     
 }
 void main()
 {
     int arr[] = {4,1,3,2,5};
-    int size = 5;
+    int size = sizeof(arr) / sizeof(arr[0]);
     printf("The sorted array is:\n");
     sort(arr,size);
+    printArray(arr,size);
     
 }
