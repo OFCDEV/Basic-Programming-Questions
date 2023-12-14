adab81c97eb8ea67d0c6fb9a1a907fd43a7069e8
Debasish Bhola <devop69@gmail.com>
2023-12-14 20:03:44 +0530

    main method added

--- binarySearch.c
+++ binarySearch.c
@@ -16,5 +16,13 @@ int binarySearch(int arr[], int l,int r ,int x){
     return -1;
 }
 void main(){
-    
+    int arr[] = { 2, 3, 4, 10, 40 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int x = 10;
+    int result = binarySearch(arr, 0, n - 1, x);
+    (result == -1) ? printf("Element is not present"
+                            " in array")
+                   : printf("Element is present at "
+                            "index %d",
+                            result);
 }
