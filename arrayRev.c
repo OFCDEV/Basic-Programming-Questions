50a25a2dbdaf5aa16ea74fc7ca5d8432e5e1a261
Debasish Bhola <devop69@gmail.com>
2023-12-14 13:52:40 +0530

    printArray func added

--- arrayRev.c
+++ arrayRev.c
@@ -9,6 +9,14 @@ void arrayRev(int arr[],int start, int end){
     }
     
 }
+void printArray(int arr[], int size){
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d",arr[i]);
+        printf("\n");
+    }
+    
+}
 
 void main(){
     int arr[] = {1,2,3,4,5,6};
