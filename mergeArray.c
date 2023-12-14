22658f4cea8933543a05ab14d52be267e8ecb04e
Debasish Bhola <devop69@gmail.com>
2023-12-14 19:10:29 +0530

    mergeTwo func added

--- mergeArray.c
+++ mergeArray.c
@@ -1,4 +1,22 @@
 #include<stdio.h>
+void mergeTwo(int a1[],int a2[],int m,int n,int res[]){
+    int i=0,j=0,k=0;
+    while(i<m && j<n){
+        if (a1[i]<a2[j])
+        {
+            res[k++]=a1[i++];
+        }else{
+            res[k++] = a2[j++];
+        }
+        while(i<m){
+            res[k++]=a1[i++];
+        }
+         while(j<n){
+            res[k++]=a2[j++];
+        }
+        
+    }
+}
 void main(){
     
 }
