2995675b2334c7f85a24fa5887631b5cf65a1d84
Debasish Bhola <devop69@gmail.com>
2023-12-14 18:26:30 +0530

    sort() method completed

--- bubbleSort.c
+++ bubbleSort.c
@@ -1,4 +1,20 @@
-#include<stdio.h>
-void main(){
-    
+#include <stdio.h>
+void sort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j - 1];
+                arr[j - 1] = temp;
+            }
+        }
+    }
+}
+void main()
+{
+
 }
