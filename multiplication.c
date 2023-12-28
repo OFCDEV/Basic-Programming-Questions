69dc7a137a3f7c8d1b9e120e78c9d55c45532d11
Debasish Bhola <devop69@gmail.com>
2023-12-28 23:37:16 +0530

    Completed

--- multiplication.c
+++ multiplication.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int multiplication(int range, int num){
     int mul;
-    for (int i = 0; i < range; i++)
+    for (int i = 0; i <= range; i++)
     {
         mul = num*i;
         printf("%d * %d = %d",num,i,mul);
@@ -10,5 +10,8 @@ int multiplication(int range, int num){
     
 }
 void main(){
-    
+    int range=10;
+    int num=5;
+    int res = multiplication(range,num);
+    printf(res);
 }
