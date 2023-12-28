80cc0f633cbcf22a6f84bca7bdd26fef4f4c1ce7
Debasish Bhola <devop69@gmail.com>
2023-12-28 23:09:00 +0530

    Main method completed

--- sumOfNnum.c
+++ sumOfNnum.c
@@ -1,4 +1,16 @@
-#include<stdio.h>
-void main(){
-    
+#include <stdio.h>
+
+int main()
+{
+
+	int i, s = 0;
+	int n;
+    printf("Enter the range:\n");
+    scanf("%d",&n);
+
+	for (i = 0; i <= n; i++) {
+		s += i;
+	}
+	printf("Sum = %d", s);
+	return 0;
 }
